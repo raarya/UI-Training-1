e4db77ffac3414eb226e3bcbe6a4d99014b83221
Arya <rajarya@deloitte.com>
2019-05-28 01:35:50 +0530

    JS commit

---
+++ javascript/second.js
@@ -0,0 +1,57 @@
+/**************************
+ *      Condition 
+ * ************************/
+
+var a = 10
+undefined
+var b = 10
+undefined
+a == b //normal "double equal to" comparison operator only for data values, not of data types
+true
+a === b // "tripple equal to" is a comparison operator of  data values, and then of data types
+true
+var c = "10"
+undefined
+a == b
+true
+a == c
+true
+a === c
+false
+
+    = is assignmnent
+        == is comparistion
+            === is compare value and datatype
+
+true == 0
+false
+true == 1
+true
+true === 1
+false
+false == 0
+true
+false === 0
+false
+
+//there are 2 types of data values in JS : truthy and falsey
+Truthy
+0 > true
+
+Falsey
+0 <=
+    0 - 1 - 454 false null undefined
+
+null = we can assign any variable as null
+undefined = we should not assign any variable as undefined
+
+var tuuu
+undefined
+tuuu
+undefined
+var a = null
+undefined
+var a = 10
+undefined
+a
+10
