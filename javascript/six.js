44b6189c6c4e3345c7c1c0989a5ee1d92e8c34e2
Arya <rajarya@deloitte.com>
2019-05-29 23:42:23 +0530

    JS commit

---
+++ javascript/six.js
@@ -0,0 +1,118 @@
+Math.random() //generates random number; JS is case sensitive, so use M only.. not "m"...
+0.48801312599556756
+Math.random()
+0.19041957440385282
+Math.random()
+0.33046497191922564
+Math.random()
+0.9848725341672382
+Math.random()
+0.56729323521496
+Math.random()
+0.7555597394383051
+Math.random()*100
+17.21083378073127
+Math.random()*100
+55.000810779622576
+Math.random()*100
+16.364769146667424
+Math.random()*100
+30.624893183835656
+Math.random()*1000000
+420536.06276967836
+Math.random()*1000000
+471607.2883643105
+Math.random()*1000000
+309555.52194970305
+Math.floor(309555.52194970305) //floor command gives a number representing the nearest integer when rounding downwards
+309555
+Math.floor(1.4)
+1
+Math.floor(1.6)
+1
+Math.floor(1.9)
+1
+Math.floor(-4.9) //very imp example with negative value
+-5
+Math.ceil(1.9) //Round a number upward to its nearest integer
+2
+Math.ceil(1.3)
+2
+Math.ceil(1.1)
+2
+Math.round(1.3) //Round a number to the nearest integer; 5 and above after decimal means increase by 1
+1
+Math.round(1.7)
+2
+
+Math.PI
+3.141592653589793
+Math.floor(Math.random()*1000000)
+783106
+Math.floor(Math.random()*1000000)
+785851
+Math.floor(Math.random()*1000000)
+824068
+Math.floor(Math.random()*1000000)
+190547
+Math.floor(Math.random()*1000000)
+161899
+
+Math.trunc(2.99) //Return the integer part of a number; This method will NOT round the number up/down to the nearest ingeger, but simply remove the decimals.
+2
+Math.trunc(2.19)
+2
+Math.trunc(2.00)
+2
+Math.pow(2,2)
+4
+Math.pow(3,2)
+9
+Math.sqrt(4)
+2
+Math.sqrt(9)
+3
+Math.sqrt(9987)
+99.93497886125758
+Math.min(1,4,6,7,0,4)
+0
+Math.max(1,4,6,7,0,4)
+7
+Math.cos(1)
+0.5403023058681398
+Math.sin(1)
+0.8414709848078965
+Math.cos(-1)
+0.5403023058681398
+Math.sin(90)
+0.8939966636005579
+Math.cos(90)
+-0.4480736161291702
+Math.cos(0)
+1
+Math.cos(90)
+-0.4480736161291702
+Math.tan(45)
+1.6197751905438615
+Math.log(1)
+0
+Math.log(1.1)
+0.09531017980432493
+
+
+var a = "10"
+undefined
+var b = "10"
+undefined
+a+b
+"1010"
+parseInt(a) //parses a string and returns integer
+10
+parseInt(a) + parseInt(b)
+20
+var c = "10.998"
+undefined
+parseInt(a) + parseInt(c)
+20
+parseInt(a) + parseFloat(c)
+20.997999999999998
--- javascript/task1.js
+++ javascript/task1.js
@@ -5,3 +5,24 @@ and output = nework
 SOLUTION : 
 var exp = "newyork";
 exp = exp.slice(0,3) + exp.slice(4);
+
+Task2:
+Luck Game
+> You have generate one random number for 3 digit
+> You have take input of 3 digit
+> if user input and random number match than win else loose
+SOLUTION :
+var a = Math.floor(Math.random()*1000);
+var b = prompt('Please enter a 3 digit number');
+if(b == a) {
+ alert ('won');
+}
+else {
+    alert ('die');
+}
+
+
+Task3:
+(√2 *6)/random*100
+SOLUTION :
+(Math.sqrt(2) * 6)/(Math.random()*100)
